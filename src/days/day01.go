d74c701f0adeb299b20b94193f4de52c3e3d1b31
Erin Schnabel <schnabel@us.ibm.com>
2018-11-20 19:38:11 -0500

    day 1 part 2

--- src/days/day01.go
+++ src/days/day01.go
@@ -1,8 +1,16 @@
 package days
 
 import (
- "io"
+//  "fmt"
+  "io"
 )
+
+func toInt(b byte) (int, bool) {
+  if ( b < '0' || '9' < b ) {
+    return 0, true
+  }
+  return int(b - '0'), false
+}
 
 // The captcha requires you to review a sequence of digits (your puzzle input)
 // and find the sum of all digits that match the next digit in the list. The 
@@ -24,14 +32,14 @@ func Captcha(reader io.ByteScanner) int {
       }
       break
     }
-    if ( b < '0' || '9' < b ) {
-      continue
+
+    i, skip := toInt(b)
+    if ( skip ) {
+      continue;
     }
 
-    i := int(b - '0')
-
     if ( init ) {
-      if ( 0 <= i && i <= 9 && prev == i ) {
+      if ( prev == i ) {
         sum = prev + sum
       }
       prev = i
@@ -41,5 +49,35 @@ func Captcha(reader io.ByteScanner) int {
       init = true
     }
   }
+  return sum
+}
+
+// Now, instead of considering the next digit, it wants you to consider the 
+// digit halfway around the circular list. That is, if your list contains 10 
+// items, only include a digit in your sum if the digit 10/2 = 5 steps 
+// forward matches it. Fortunately, your list has an even number of elements.
+
+func HalfCaptcha(b []byte) int {
+  var (
+    sum int = 0
+    length int = len(b)
+    j int = length / 2
+  )
+  
+  for i := 0; i < length; i++ {
+    x := int(b[i] - '0')
+    y := int(b[j] - '0')
+    if ( x == y ) {
+      sum = sum + x
+    }
+    //fmt.Println(length, i, j, "::", x, y,  sum)
+
+    j++
+    if ( j >= length ) {
+      j = 0
+    }
+  }
+
+  //fmt.Println("---")
   return sum
 }
--- src/days/day01_test.go
+++ src/days/day01_test.go
@@ -2,7 +2,9 @@ package days
 
 import (
  "bufio"
+ "bytes"
  "fmt"
+ "io/ioutil"
  "os"
  "strings"
  "testing"
@@ -13,15 +15,30 @@ type testpair struct {
   expected int
 }
 
-var tests = []testpair {
+var tests_part1 = []testpair {
   {"1122", 3},
   {"1111", 4},
   {"1234", 0},
   {"91212129", 9},
 }
 
-func TestSampleData(t *testing.T) {
-  for _, pair := range tests {  
+var tests_part2 = []testpair {
+  {"1212", 6},
+  {"1221", 0},
+  {"123425", 4},
+  {"123123", 12},
+  {"12131415", 4},
+}
+
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func TestSampleData_part1(t *testing.T) {
+  for _, pair := range tests_part1 {  
     reader := strings.NewReader(pair.input)
     v := Captcha(reader)    
     if v != pair.expected {
@@ -34,21 +51,44 @@ func TestSampleData(t *testing.T) {
   }
 }
 
-func check(e error) {
-  if e != nil {
-    panic(e)
-  }
-}
-
-func TestInput(t *testing.T) {
+func TestInput_part1(t *testing.T) {
   f, err := os.Open("day01_input.txt")
   check(err)
   defer f.Close()
 
   reader := bufio.NewReader(f)
-  b4, err := reader.Peek(5)
+  _, err = reader.Peek(2)
   check(err)
  
   v := Captcha(reader)
-  fmt.Println("Day 1 Result", v);
+  fmt.Println("Day 1 / Part 1 Result", v);
+}
+
+func TestSampleData_part2(t *testing.T) {
+  for _, pair := range tests_part2 {
+    b := []byte(pair.input)
+    v := HalfCaptcha(b)    
+    if v != pair.expected {
+      t.Error(
+        "For", pair.input,
+        "expected", pair.expected,
+        "got", v,
+      )
+    }
+  }
+}
+
+
+func TestInput_part2(t *testing.T) {
+  content, err := ioutil.ReadFile("day01_input.txt")
+	if err != nil {
+		panic(err)
+  }
+
+  content = bytes.TrimRightFunc(content, func(r rune) bool {
+    return ( r < '0' || '9' < r )
+  })
+
+  v := HalfCaptcha(content)
+  fmt.Println("Day 1 / Part 2 Result", v)
 }
