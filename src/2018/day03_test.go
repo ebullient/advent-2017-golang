359ca69851ccd59a2e2fd8074a93a6e77880fabc
Erin Schnabel <schnabel@us.ibm.com>
2018-12-06 09:45:03 -0500

    Day 3 Part 2

--- src/2018/day03_test.go
+++ src/2018/day03_test.go
@@ -13,6 +13,7 @@ import (
 type testFabricClaim struct {
 	claims    string
 	conflicts int
+	distinct  string
 }
 
 type Square struct {
@@ -21,8 +22,8 @@ type Square struct {
 }
 
 var test_3part1 = []testFabricClaim{
-	{"#1 @ 1,3: 1x3\n#2 @ 1,3: 3x1\n#3 @ 1,3: 2x2", 3},
-	{"#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2", 4},
+	{"#1 @ 1,3: 1x3\n#2 @ 1,3: 3x1\n#3 @ 1,3: 2x2", 3, ""},
+	{"#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2", 4, "3"},
 }
 
 //var test_3part2 = []testDay2FabricBoxes{
@@ -46,36 +47,50 @@ func DefineClaim(fabric map[Square][]string, id string, start Square, width int,
 	}
 }
 
-func PlaceClaims(scanner *bufio.Scanner) int {
+func PlaceClaims(scanner *bufio.Scanner) (int, string) {
 	// the compiler panics if this fails
 	r := regexp.MustCompile(`#(\d+) @ (\d+),(\d+): (\d+)x(\d+)`)
 
+	distinct := map[string]bool{}
 	fabric := map[Square][]string{}
 	for scanner.Scan() {
 		results := r.FindStringSubmatch(scanner.Text())
+		distinct[results[1]] = true
 		DefineClaim(fabric, results[1],
 			Square{ToInt(results[2]), ToInt(results[3])},
 			ToInt(results[4]),
 			ToInt(results[5]))
 	}
 
+	// fmt.Println(fabric)
 	conflicts := 0
 	for _, value := range fabric {
-		if len(value) > 1 {
-			// fmt.Println("multiple claims at", key, "from", value)
+		length := len(value)
+		if length > 1 {
 			conflicts++
+			for _, id := range value {
+				delete(distinct, id)
+			}
 		}
 	}
 
-	return conflicts
+	id := ""
+	for key, _ := range distinct {
+		id = key
+	}
+
+	return conflicts, id
 }
 
 func TestSampleData_3part1(t *testing.T) {
 	for _, sample := range test_3part1 {
 		scanner := bufio.NewScanner(strings.NewReader(sample.claims))
-		conflicts := PlaceClaims(scanner)
+		conflicts, distinct := PlaceClaims(scanner)
 		if conflicts != sample.conflicts {
 			t.Error("For", sample.claims, "expected", sample.conflicts, "got", conflicts)
+		}
+		if distinct != sample.distinct {
+			t.Error("For", sample.claims, "expected distinct id", sample.distinct, "got", distinct)
 		}
 	}
 }
@@ -86,30 +101,8 @@ func TestInput_3part1(t *testing.T) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	conflicts := PlaceClaims(scanner)
+	conflicts, distinct := PlaceClaims(scanner)
 
 	fmt.Println("Day 4 / Part 1 Result", conflicts)
-}
-
-// --- PART 2 --------
-
-func TestSampleData_3part2(t *testing.T) {
-	//	for _, sample := range test_3part2 {
-
-	//		common := CompareBoxIds(strings.Fields(sample.input))
-
-	// expensive! but this is only a test...
-	//		if strings.Compare(sample.common, common) != 0 {
-	//			t.Error("For [", sample.input, "] expected [", sample.common, "] got [", common, "]")
-	//		}
-	//	}
-}
-
-func TestInput_3part2(t *testing.T) {
-	//	content, err := ioutil.ReadFile("day02_input.txt")
-	//	check(err)
-
-	//	common := CompareBoxIds(strings.Fields(string(content)))
-
-	//	fmt.Println("Day 3 / Part 2 Result", common)
+	fmt.Println("Day 4 / Part 2 Result", distinct)
 }
