d7470eae2c47b4f6fdc82b9bd8945d18207a1e18
Erin Schnabel <schnabel@us.ibm.com>
2018-12-14 08:45:07 -0500

    day 6

--- src/2018/day06_test.go
+++ src/2018/day06_test.go
@@ -17,6 +17,7 @@ type Point struct {
 type PointInfo struct {
 	name    string
 	nearest Point
+	md      int
 	edge    bool
 }
 
@@ -35,18 +36,36 @@ type Grid struct {
 	yMin int
 }
 
-func (g Grid) String() string {
+func PrettyArea(g Grid) string {
 	var builder strings.Builder
 	builder.WriteString(fmt.Sprintf("(%d,%d) to (%d,%d)\n", g.xMin, g.yMin, g.xMax, g.yMax))
 	for j := g.yMin; j <= g.yMax; j++ {
 		for i := g.xMin; i <= g.xMax; i++ {
 			p := g.plot[Point{i, j}].nearest
 			if p.x < 0 {
-				builder.WriteString("(...)")
+				builder.WriteString("(.......)")
 			} else {
-				builder.WriteString(fmt.Sprintf("(%d,%d)", p.x, p.y))
+				builder.WriteString(fmt.Sprintf("(%3d,%3d)", p.x, p.y))
 			}
-			builder.WriteString("\t")
+			builder.WriteString(" ")
+		}
+		builder.WriteString("\n")
+	}
+	return builder.String()
+}
+
+func PrettySafe(g Grid, threshold int) string {
+	var builder strings.Builder
+	builder.WriteString(fmt.Sprintf("(%d,%d) to (%d,%d)\n", g.xMin, g.yMin, g.xMax, g.yMax))
+	for j := g.yMin; j <= g.yMax; j++ {
+		for i := g.xMin; i <= g.xMax; i++ {
+			a := g.plot[Point{i, j}].md
+			if a < threshold {
+				builder.WriteString("*")
+			} else {
+				builder.WriteString(" ")
+			}
+			builder.WriteString(" ")
 		}
 		builder.WriteString("\n")
 	}
@@ -69,9 +88,9 @@ func Abs(i int) int {
 	return i
 }
 
-var MD_CONFLICT = Point{-2, -2}
+var MdConflict = Point{-2, -2}
 
-func FindNearest(origin Point, grid Grid, coords []Point) {
+func FindMD(origin Point, grid Grid, coords []Point) {
 	info := grid.plot[origin]
 
 	if origin.x == grid.xMin ||
@@ -81,21 +100,30 @@ func FindNearest(origin Point, grid Grid, coords []Point) {
 		info.edge = true
 	}
 
-	if info.name == "" { // don't bother with coordinates in the input set
-		minDistance := 100 // arbitrary big number
-		nearest := origin  // arbitrary not 0,0
+	minDistance := 100 // arbitrary big number
+	nearest := origin  // start with this
 
-		for _, i := range coords {
-			md := Abs(origin.x-i.x) + Abs(origin.y-i.y)
+	for _, i := range coords {
+		md := Abs(origin.x-i.x) + Abs(origin.y-i.y)
+
+		// accumulate the md to all points in the input set
+		info.md += md
+
+		// find the nearest coordinate when not in the input set
+		if info.name == "" {
 			if md < minDistance {
 				nearest = i
 				minDistance = md
 			} else if md == minDistance {
-				nearest = MD_CONFLICT
+				nearest = MdConflict
 			}
 		}
+	}
 
-		// remember the nearest coordinate
+	// remember the nearest coordinate when not in the input set
+	// (input points are excluded, as they are always shortest
+	// for something else)
+	if info.name == "" {
 		info.nearest = nearest
 	}
 
@@ -111,7 +139,7 @@ func PlotPoints(input []Point) Grid {
 	grid.yMin = input[0].y
 
 	for idx, pt := range input {
-		grid.plot[pt] = PointInfo{fmt.Sprintf("c%d", idx), pt, false}
+		grid.plot[pt] = PointInfo{fmt.Sprintf("c%d", idx), pt, 0, false}
 
 		// Define the edges of the grid
 		if pt.x < grid.xMin {
@@ -128,20 +156,25 @@ func PlotPoints(input []Point) Grid {
 		}
 	}
 
-	// go through points of grid, find nearest
+	// go through points of grid, find Manhattan Distance
 	for i := grid.xMin; i <= grid.xMax; i++ {
 		for j := grid.yMin; j <= grid.yMax; j++ {
-			FindNearest(Point{i, j}, grid, input)
+			FindMD(Point{i, j}, grid, input)
 		}
 	}
 
 	return grid
 }
 
-func CountNearest(grid Grid) int {
+func CountNearestSafe(grid Grid, threshold int) (int, int) {
 	tally := map[Point][]PointInfo{}
+	safeArea := 0
 	for _, i := range grid.plot {
 		tally[i.nearest] = append(tally[i.nearest], i)
+
+		if i.md < threshold {
+			safeArea++
+		}
 	}
 
 	maxArea := 0
@@ -158,7 +191,7 @@ func CountNearest(grid Grid) int {
 		}
 	}
 
-	return maxArea
+	return maxArea, safeArea
 }
 
 func ParsePoints(list []string) []Point {
@@ -183,12 +216,13 @@ func ParsePoints(list []string) []Point {
 
 func TestSampleData_6part1(t *testing.T) {
 	grid := PlotPoints(test_6part1)
-	largest := CountNearest(grid)
-	//	fmt.Println(grid)
-	//	fmt.Println(largest)
+	maxArea, safeArea := CountNearestSafe(grid, 32)
 
-	if largest != 17 {
-		t.Error("Expected largest area to be 17. Got", largest)
+	if maxArea != 17 {
+		t.Error("Expected largest area to be 17. Got", maxArea)
+	}
+	if safeArea != 16 {
+		t.Error("Expected largest area to be 16. Got", safeArea)
 	}
 }
 
@@ -202,9 +236,10 @@ func TestInput_6(t *testing.T) {
 	pts := ParsePoints(list)
 
 	grid := PlotPoints(pts)
-	largest := CountNearest(grid)
-	//	fmt.Println(grid)
-	//	fmt.Println(largest)
+	maxArea, safeArea := CountNearestSafe(grid, 10000)
+	// fmt.Println(PrettyArea(grid))
+	// fmt.Println(PrettySafe(grid, 10000))
 
-	fmt.Println("Day 6 / Part 1 Result", largest)
+	fmt.Println("Day 6 / Part 1 Result", maxArea)
+	fmt.Println("Day 6 / Part 2 Result", safeArea)
 }
