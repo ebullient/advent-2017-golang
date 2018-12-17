8ee447b7a27c56160b47ddb6759bd117b4d10295
Erin Schnabel <schnabel@us.ibm.com>
2018-12-17 11:22:37 -0500

    day 8 part 2

--- src/2018/day08_test.go
+++ src/2018/day08_test.go
@@ -9,15 +9,22 @@ import (
 )
 
 type Tree struct {
-	nodes       map[int]Node
-	totalNodes  int
-	sumMetadata int
+	nodes      map[int]Node
+	totalNodes int
+	total      int
 }
 
 type Node struct {
 	id       int
-	children []int
+	children map[int]int
 	metadata []int
+	value    int
+}
+
+func PrettyTree(tree *Tree) {
+	for k, n := range tree.nodes {
+		fmt.Println(k, ": ", n.children, ", ", n.metadata)
+	}
 }
 
 func ReadInt(scanner *bufio.Scanner) int {
@@ -36,7 +43,7 @@ func ReadNode(tree *Tree, scanner *bufio.Scanner) Node {
 
 	// HEADER: num children, num metadata
 	numChildren := ReadInt(scanner)
-	node.children = make([]int, numChildren)
+	node.children = map[int]int{}
 
 	numMetadata := ReadInt(scanner)
 	node.metadata = make([]int, numMetadata)
@@ -50,10 +57,24 @@ func ReadNode(tree *Tree, scanner *bufio.Scanner) Node {
 	// Metadata for this node
 	for i = 0; i < numMetadata; i++ {
 		node.metadata[i] = ReadInt(scanner)
-		tree.sumMetadata += node.metadata[i]
+
+		// Part 1: all metadata
+		tree.total += node.metadata[i]
+
+		// Part 2
+		if numChildren == 0 {
+			node.value += node.metadata[i]
+		} else {
+			x := node.metadata[i] - 1
+			if x >= 0 && x < numChildren {
+				id := node.children[x]
+				value := tree.nodes[id].value
+				node.value += value
+			}
+		}
 	}
 
-	// store node in tree
+	// store node in tree (extraneous: test/debug)
 	tree.nodes[node.id] = node
 	return node
 }
@@ -71,19 +92,16 @@ func ParseTree(scanner *bufio.Scanner) *Tree {
 func TestSampleData_8part1(t *testing.T) {
 	input := "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
 	verify := [][]int{
-		{2, 3, 4},
-		{0, 3, 33},
-		{1, 1, 2},
-		{0, 1, 99},
+		{2, 3, 4, 66},  // A
+		{0, 3, 33, 33}, // B
+		{1, 1, 2, 0},   // C
+		{0, 1, 99, 99}, // D
 	}
 
 	scanner := bufio.NewScanner(strings.NewReader(input))
 	tree := ParseTree(scanner)
 	fmt.Println(tree)
 
-	if tree.sumMetadata != 138 {
-		t.Error("Expected metadata to equal 138, got", tree.sumMetadata)
-	}
 	if tree.totalNodes != 4 {
 		t.Error("Expected 4 nodes, got", tree.totalNodes)
 	}
@@ -94,6 +112,7 @@ func TestSampleData_8part1(t *testing.T) {
 		if len(n.metadata) != verify[i][1] {
 			t.Error("Expected", verify[i][1], "metadata, got", len(n.metadata))
 		}
+		// Part 1 sub-parts
 		sum := 0
 		for _, i := range n.metadata {
 			sum += i
@@ -101,6 +120,14 @@ func TestSampleData_8part1(t *testing.T) {
 		if sum != verify[i][2] {
 			t.Error("Expected numChildren to equal", verify[i][2], "got", sum)
 		}
+		// Part 2
+		if n.value != verify[i][3] {
+			t.Error("Expected sum of node metadata to equal", verify[i][3], "got", n.value)
+		}
+	}
+	// Part 1 total
+	if tree.total != 138 {
+		t.Error("Expected metadata to equal 138, got", tree.total)
 	}
 }
 
@@ -119,12 +146,6 @@ func TestInput_8part1(t *testing.T) {
 	defer elapsed("TestInput_8part1")() // time execution of the rest
 
 	tree := ParseTree(scanner)
-	fmt.Println("Day 8 / Part 1 Result", tree.sumMetadata)
-}
-
-func TestSampleData_8part2(t *testing.T) {
-}
-
-func TestInput_8part2(t *testing.T) {
-	//	fmt.Println("Day 8 / Part 2 Result", work.ticks, strings.Join(result, ""))
+	fmt.Println("Day 8 / Part 1 Result", tree.total)
+	fmt.Println("Day 8 / Part 2 Result", tree.nodes[0].value)
 }
